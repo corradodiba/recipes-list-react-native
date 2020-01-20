286e7be27cef88be565435b8e6d65b1c2c4a7011
Corrado Dibattista <corradodiba@gmail.com>
2020-01-21 00:12:40 +0100

    first basic implementation to stack navigation

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
-const CategoriesScreen = () => {
+const CategoriesScreen = ({ navigation }) => {
   return (
     <View style={styles.screen}>
       <Text>This is the Categories Screen</Text>
+      <Button
+        title="Go to Category Meal"
+        onPress={() => navigation.navigate("CategoryMeal")}
+      />
     </View>
   );
 };
--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
-const CategoryMealScreen = () => {
+const CategoryMealScreen = ({ navigation }) => {
   return (
     <View style={styles.screen}>
       <Text>This is the Category Meal Screen</Text>
+      <Button
+        title="Go to Meal Detail"
+        onPress={() => navigation.navigate("MealDetail")}
+      />
     </View>
   );
 };
